d077db7376d19bf8e16afe4eb194e098fc7e8ce0
info-mic-info <jobsocial94@gmail.com>
2023-02-19 20:02:06 +0100

    add db.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,10 +15,12 @@
 
 </head>
 
-<body>
+<body> 
+    @include('partials.header')
     <div class="container">
-        <h1>Ciao</h1>
+        <h1>Ciao</h1>  
     </div>
+    @include('partials.footer')
 </body>
 
 </html>
